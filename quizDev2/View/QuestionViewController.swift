306f4777fa21020d3e8c581a930997f16a8be692
aduartee <arthurduartealves99@gmail.com>
2024-05-12 21:18:18 -0300

    feat: Initial code with call an API of quesions

--- quizDev2/View/QuestionViewController.swift
+++ quizDev2/View/QuestionViewController.swift
@@ -6,21 +6,38 @@
 //
 
 import UIKit
+import Foundation
 
 class QuestionViewController: UIViewController {
 
     @IBOutlet weak var titleQuestion: UILabel!
+    @IBOutlet var buttonResponses: [UIButton]!
     let numberQuestion: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         styleView()
+        makeRequest()
     }
     
+    @IBAction func getResponseOnTap(_ sender: UIButton) {
+        print("Tag of button is: \(sender.tag)")
+    }
     func styleView() {
         navigationItem.hidesBackButton = true
         titleQuestion.numberOfLines = 0
         titleQuestion.text =  questionsArr[numberQuestion].label
+    }
+    
+    private func makeRequest() {
+        let url = URL(string:"https://json-questions.vercel.app/questions")!
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            
+            print(String(describing: response))
+        }
+        
+        task.resume()
         
     }
     
