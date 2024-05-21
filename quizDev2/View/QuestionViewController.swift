cc7c4ac07773aedecb3cf9a6d277b9a7c5dfb243
aduartee <arthurduartealves99@gmail.com>
2024-05-21 12:47:39 -0300

    Implemented the logic to skip to next question and make Delay when switch question

--- quizDev2/View/QuestionViewController.swift
+++ quizDev2/View/QuestionViewController.swift
@@ -14,10 +14,12 @@ class QuestionViewController: UIViewController {
     @IBOutlet var buttonResponses: [UIButton]!
     var numberQuestion: Int = 0
     var points: Int = 0
-    
+    var countActualQuestion: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        styleView()
+        navigationItem.hidesBackButton = true
+        titleQuestion.numberOfLines = 0
+        loadLayout()
     }
     
     @IBAction func getResponseOnTap(_ sender: UIButton) {
@@ -25,27 +27,38 @@ class QuestionViewController: UIViewController {
             DispatchQueue.main.async {
                 let correctResponse = questions[self.numberQuestion].response
                 if sender.tag == correctResponse {
-                    self.numberQuestion += 1
                     self.points += 1
-                } else {
+                    sender.backgroundColor = .green
                     
+                } else {
+                    sender.backgroundColor = .red
                 }
                 
+                let isLatIndex: Bool = self.countActualQuestion == questions.count ? true : false
+                
+                if !isLatIndex {
+                    self.numberQuestion += 1
+                    Timer.scheduledTimer(timeInterval: 0.9, target: self, selector: #selector(self.loadLayout), userInfo: nil, repeats: false)
+                } else {
+                    print("Hello World")
+                }
             }
             
         }
     }
     
-    func styleView() {
-        navigationItem.hidesBackButton = true
-        titleQuestion.numberOfLines = 0
+    @objc func loadLayout() {
         makeRequest { (questions) in
             DispatchQueue.main.async {
                 self.titleQuestion.text = questions[self.numberQuestion].title
                 for button in self.buttonResponses {
+                    button.sizeToFit()
+                    button.backgroundColor = UIColor(red: 75/255.0, green: 140/255.0, blue: 225/255.0, alpha: 1.0)
                     let newTitleQuestion = questions[self.numberQuestion].questions[button.tag]
                     button.setTitle(newTitleQuestion, for: .normal)
+                    button.layer.cornerRadius = 20
                 }
+                self.countActualQuestion += 1
             }
         }
     }
