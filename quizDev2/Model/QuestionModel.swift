9e0f4cc4f322ab68f75e769aa2be80eed59890e6
aduartee <arthurduartealves99@gmail.com>
2024-05-30 18:12:35 -0300

    feat: Started the logic to sort questions of the JSON endpoint

--- quizDev2/Model/QuestionModel.swift
+++ quizDev2/Model/QuestionModel.swift
@@ -12,3 +12,20 @@ struct QuestionModel: Codable {
     let questions: [String]
     let response: Int
 }
+
+//struct ShuffledQuestion {
+//    let title: String
+//    let questions: [String]
+//    let response: Int
+//}
+//
+//func shuffledQuestions(_ question: QuestionModel) -> ShuffledQuestion {
+//    var indexedQuestion = question.questions.enumerated().map { (index, element) in (index: index, question: element)
+//    }
+//    
+//    indexedQuestion.shuffle()
+//    let shuffledQuestions = indexedQuestion.map { $0.question}
+//    let newCorrectIndex = indexedQuestion.first { $0.index == question.response}
+//    
+//    
+//}
