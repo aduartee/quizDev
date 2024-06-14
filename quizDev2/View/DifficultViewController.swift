5b9bd77306901ea68006aeb6afc5d65af9142833
aduartee <arthurduartealves99@gmail.com>
2024-06-14 18:29:03 -0300

    feat: Making View for Difficult

---
+++ quizDev2/View/DifficultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DifficultViewController.swift
+//  quizDev2
+//
+//  Created by Arthur Duarte on 14/06/24.
+//
+
+import UIKit
+
+class DifficultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
